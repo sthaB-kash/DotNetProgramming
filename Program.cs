f40b909e522bd679675811a639627bd03430f6c4
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-15 20:52:58 +0545

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
