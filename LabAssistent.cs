dee9ba8c2164d92732f875c21e1aa5f32df26114
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-15 22:04:11 +0545

    create LabAssistant class inherited from Employee

--- LabAssistent.cs
+++ LabAssistent.cs
@@ -1,12 +1,22 @@
 using System;
 
-namespace lab_5 {
-  public class LabAssistant:Employee {
-    public LabAssistant(int id, string name, double sal):base(id:id, name:name, salary_per_day:sal){
+namespace lab_5
+{
+  public class LabAssistant : Employee
+  {
+    public LabAssistant(int id, string name, double sal) : base(id: id, name: name, salary_per_day: sal)
+    {
 
     }
-    public override double calc_salary() {
-      return this.salary_per_day *30;
+    public override double calc_salary()
+    {
+      return this.salary_per_day * 30;
+    }
+
+    public override void display()
+    {
+      base.display();
+      Console.WriteLine("(Lab Assistant)");
     }
   }
 }
