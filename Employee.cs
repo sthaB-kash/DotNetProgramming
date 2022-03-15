1a46d8826c627f4cbbbab3374fe1337e8592c7a3
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-15 21:23:08 +0545

    create employee abastract class

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace lab_5
+{
+  public abstract class Employee
+  {
+    protected int id;
+    string name;
+    protected double salary_per_day;
+
+    public Employee(int id, string name, double salary_per_day)
+    {
+      this.id = id;
+      this.name = name;
+      this.salary_per_day = salary_per_day;
+    }
+    public virtual void display()
+    {
+      Console.WriteLine("Employee Details: {0} \nName: {1}\nSalary: {2}", this.id, this.name, this.salary_per_day);
+    }
+
+    public abstract double calc_salary();
+  }
+
+}
