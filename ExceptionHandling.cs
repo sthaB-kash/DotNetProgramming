8aaa672653bae6520832daad02e53a8b6289d10b
Bikash Shrestha <shresthabikash2073@gmail.com>
2022-06-30 00:09:09 +0545

    divide by zero exception

--- ExceptionHandling.cs
+++ ExceptionHandling.cs
@@ -18,14 +18,25 @@ namespace dotnet_hello_world {
         }
 
         public static void Main(String [] args) {
-            ExceptionExample obj = new ExceptionExample();
-            try{
-                Console.Write("Enter you name: ");
-                obj.Name = Console.ReadLine();
-            }catch (Exception e) {
+            // ExceptionExample obj = new ExceptionExample();
+            // try{
+            //     Console.Write("Enter you name: ");
+            //     obj.Name = Console.ReadLine();
+            // }catch (Exception e) {
+            //     Console.WriteLine(e.Message);
+            // }finally {
+            //     Console.WriteLine("Hello, " + obj.Name);
+            // }
+
+            // divide by zero exception
+            int a = 10, b = 0, rel = 0;
+            
+            try {
+                rel = a / b;
+            } catch (DivideByZeroException e) {
                 Console.WriteLine(e.Message);
-            }finally {
-                Console.WriteLine("Hello, " + obj.Name);
+            } finally {
+                Console.WriteLine("Result: {0}", rel);
             }
 
         }
