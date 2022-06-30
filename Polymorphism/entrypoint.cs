e9bed68573e175200cbe0c0181a03da88c4a98ad
Bikash Shrestha <shresthabikash2073@gmail.com>
2022-06-30 21:18:39 +0545

    polymorphism

--- Polymorphism/entrypoint.cs
+++ Polymorphism/entrypoint.cs
@@ -1,34 +1,42 @@
-using System;
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
+// using System.Text;
 
-namespace polymorphism_example {
-    class Program {
-        static void Main() {
-            // method overloading
-            // PrintData p = new PrintData();
-            // p.print(10);
-            // p.print(10.5f);
-            // p.print(10.5);
-            // p.print("Hello World");
-            // p.print('A');
+// namespace polymorphism_example {
+//     class Program {
+//         static void Main() {
+//             // method overloading
+//             // PrintData p = new PrintData();
+//             // p.print(10);
+//             // p.print(10.5f);
+//             // p.print(10.5);
+//             // p.print("Hello World");
+//             // p.print('A');
 
-            //  OPERATOR OVERLOADING
-            // Box b1 = new Box(10, 20, 30);
-            // Box b2 = new Box(20, 30, 40);
-            // b1.printVolume();
-            // b2.printVolume();
-            // Box b3 = b1 + b2;
-            // b3.printVolume();  
+//             //  OPERATOR OVERLOADING
+//             // Box b1 = new Box(10, 20, 30);
+//             // Box b2 = new Box(20, 30, 40);
+//             // b1.printVolume();
+//             // b2.printVolume();
+//             // Box b3 = b1 + b2;
+//             // b3.printVolume();  
 
 
-            // VIRTUAL FUNCTION 
-            List <Shape> shapes = new List<Shape>();
-            shapes.Add(new Rectangle(10, 20));
-            shapes.Add(new Triangle(10, 20));
+//             // VIRTUAL FUNCTION 
+//             // List <Shape> shapes = new List<Shape>();
+//             // shapes.Add(new Rectangle(10, 20));
+//             // shapes.Add(new Triangle(10, 20));
 
-            foreach(Shape s in shapes) {
-                Console.WriteLine("Area: {0}", s.area());
-            }
-        }
-    }
-}
+//             // foreach(Shape s in shapes) {
+//             //     Console.WriteLine("Area: {0}", s.area());
+//             // }
+
+//             String str = new String("bikash");
+//             Console.WriteLine(str+ " shrestha");   
+
+//             StringBuilder sb = new StringBuilder("tst");
+//             sb.Append("bikash");
+//             Console.WriteLine(sb);
+//       }
+//     }
+// }
